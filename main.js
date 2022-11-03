f2d79a12ea520921e329729122db33d56006e358
Jonathan Baer <jonathanbaeremails@gmail.com>
2022-11-03 18:24:44 -0300

    finalizado formulario, seguindo com o desenvolvimento do CRUD - ETAPA 1: Create

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-'use strict'
+"use strict";
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+const openModal = () =>
+  document.getElementById("modal").classList.add("active");
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () =>
+  document.getElementById("modal").classList.remove("active");
 
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+const tempClient = {
+  nome: "baer",
+  email: "baer@gmail.com",
+  contato: "16996467556",
+  cidade: "Araraquara",
+  processo: "Polimento",
+};
 
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+//CRUD
+
+const createClient = (client) => {
+  const db_client = JSON.parse(localStorage.getItem('db_client'));
+  console.log(db_client);
+  db_client.push(client);
+  localStorage.setItem("db_client", JSON.stringify(db_client));
+};
+
+//Eventos Modal
+
+document
+  .getElementById("cadastrarCliente")
+  .addEventListener("click", openModal);
+
+document.getElementById("modalClose").addEventListener("click", closeModal);
