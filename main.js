775ce036571f3789bd70654d90815189da948d54
Jonathan Baer <jonathanbaeremails@gmail.com>
2022-11-04 16:52:09 -0300

    crud finalizado

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
 "use strict";
 
+// Janela para novo registro
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
 const closeModal = () =>
   document.getElementById("modal").classList.remove("active");
 
+// Dados cadastrais JSON
 const tempClient = {
-  nome: "baer",
-  email: "baer@gmail.com",
+  nome: "bryan",
+  email: "bryan@gmail.com",
   contato: "16996467556",
   cidade: "Araraquara",
   processo: "Polimento",
 };
 
-//CRUD
+// Dados inseridos e coletados apartir do localstorage
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("db_client")) ?? [];
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem("db_client", JSON.stringify(dbClient));
 
+
+
+  //CRUD - create , reade , update , delete
+
+
+//CRUD - delete
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index,1)
+  setLocalStorage(dbClient)
+}
+
+//CRUD - update
+const updateCliente = (index, client) => {
+  const dbClient = readClient();
+  dbClient[index] = client;
+  setLocalStorage(dbClient)
+};
+
+//CRUD - read
+const readClient = () => getLocalStorage();
+
+//CRUD - create
 const createClient = (client) => {
-  const db_client = JSON.parse(localStorage.getItem('db_client'));
-  console.log(db_client);
-  db_client.push(client);
-  localStorage.setItem("db_client", JSON.stringify(db_client));
+  const dbClient = getLocalStorage();
+  dbClient.push(client);
+  setLocalStorage(dbClient);
 };
 
 //Eventos Modal
