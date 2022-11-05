87e236a24bc234b728c67deca7af1669091c088b
Jonathan Baer <jonathanbaeremails@gmail.com>
2022-11-05 18:36:32 -0300

    CRUD concluido - ETAPA 2: javascrip e json

--- main.js
+++ main.js
@@ -4,17 +4,12 @@
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
-const closeModal = () =>
+const closeModal = () => {
+  clearFields();
   document.getElementById("modal").classList.remove("active");
+};
 
 // Dados cadastrais JSON
-const tempClient = {
-  nome: "bryan",
-  email: "bryan@gmail.com",
-  contato: "16996467556",
-  cidade: "Araraquara",
-  processo: "Polimento",
-};
 
 // Dados inseridos e coletados apartir do localstorage
 const getLocalStorage = () =>
@@ -22,23 +17,20 @@ const getLocalStorage = () =>
 const setLocalStorage = (dbClient) =>
   localStorage.setItem("db_client", JSON.stringify(dbClient));
 
-
-
-  //CRUD - create , reade , update , delete
-
+//CRUD - create , reade , update , delete
 
 //CRUD - delete
 const deleteClient = (index) => {
-  const dbClient = readClient()
-  dbClient.splice(index,1)
-  setLocalStorage(dbClient)
-}
+  const dbClient = readClient();
+  dbClient.splice(index, 1);
+  setLocalStorage(dbClient);
+};
 
 //CRUD - update
 const updateCliente = (index, client) => {
   const dbClient = readClient();
   dbClient[index] = client;
-  setLocalStorage(dbClient)
+  setLocalStorage(dbClient);
 };
 
 //CRUD - read
@@ -51,6 +43,104 @@ const createClient = (client) => {
   setLocalStorage(dbClient);
 };
 
+// Interacao com o layout
+
+const clearFields = () => {
+  const fields = document.querySelectorAll(".modal-field");
+  fields.forEach((field) => (field.value = ""));
+  document.getElementById("nome").dataset.index = "new";
+};
+
+const saveClient = () => {
+  if (isValidFields()) {
+    const client = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      contato: document.getElementById("contato").value,
+      cidade: document.getElementById("cidade").value,
+      processo: document.getElementById("processo").value,
+    };
+    const index = document.getElementById("nome").dataset.index;
+    if (index == "new") {
+      createClient(client);
+      updateTable();
+      closeModal();
+    } else {
+      updateCliente(index, client);
+      updateTable();
+      closeModal();
+    }
+  }
+};
+
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
+
+const createRow = (client, index) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+      <td>${client.nome}</td>
+      <td>${client.email}</td>
+      <td>${client.contato}</td>
+      <td>${client.cidade}</td>
+      <td>${client.processo}</td>
+      <td>
+          <button type="button" class="button green" id="editar-${index}">Editar</button>
+          <button type="button" class="button red" id="delete-${index}">Excluir</button>
+       </td>
+   `;
+  document.querySelector("#tbClient>tbody").appendChild(newRow);
+};
+
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tbClient>tbody tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
+};
+
+const updateTable = () => {
+  const dbClient = readClient();
+  clearTable();
+  dbClient.forEach(createRow);
+};
+
+const fillFields = (client) => {
+  document.getElementById("nome").value = client.nome;
+  document.getElementById("email").value = client.email;
+  document.getElementById("contato").value = client.contato;
+  document.getElementById("cidade").value = client.cidade;
+  document.getElementById("processo").value = client.processo;
+  document.getElementById("nome").dataset.index = client.index;
+};
+
+const editClient = (index) => {
+  const client = readClient()[index];
+  client.index = index;
+  fillFields(client);
+  openModal();
+};
+
+const editDelete = (event) => {
+  if (event.target.type == "button") {
+    const [action, index] = event.target.id.split("-");
+
+    if (action == "editar") {
+      editClient(index);
+    } else {
+      const client = readClient()[index];
+      const response = confirm(
+        `Deseja realmente excluir o cliente ${client.nome}`
+      );
+      if (response) {
+        deleteClient(index);
+        updateTable();
+      }
+    }
+  }
+};
+
+updateTable();
+
 //Eventos Modal
 
 document
@@ -58,3 +148,7 @@ document
   .addEventListener("click", openModal);
 
 document.getElementById("modalClose").addEventListener("click", closeModal);
+
+document.getElementById("salvar").addEventListener("click", saveClient);
+
+document.querySelector("#tbClient>tbody").addEventListener("click", editDelete);
